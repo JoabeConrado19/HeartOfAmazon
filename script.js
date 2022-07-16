82e21ec9de0048a586a8c75fe8af39f25f2e101b
JoabeConrado19 <joabe.conrado19@gmail.com>
2022-07-16 09:05:52 -0400

    Atualização Header Responsivo

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let mobileMenu = document.querySelector(".menu-toggle");
 let navList = document.querySelector(".nav-list");
-let navTextLi = document.querySelector(".nav-text-li");
+let Categorias = document.querySelector(".drop");
+let Destaques = document.querySelector(".drop2");
+
 
 function click (){
     navList.classList.toggle("active")
 }
+function click2 (){
+    Categorias.classList.toggle("dropdown")
+}
+function click3 (){
+    Destaques.classList.toggle("dropdown")
+}
 mobileMenu.addEventListener("click", click)
+Categorias.addEventListener("click", click2)
+Destaques.addEventListener("click", click3)
