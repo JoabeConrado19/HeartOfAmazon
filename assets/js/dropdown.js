cbe220720fd95f01d64e5052827bfec51d2c08ac
JoabeConrado19 <joabe.conrado19@gmail.com>
2022-07-18 16:35:02 -0400

    Inicio shopping cart

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -2,10 +2,14 @@ let mobileMenu = document.querySelector(".menu-toggle");
 let navList = document.querySelector(".nav-list");
 let Categorias = document.querySelector(".drop");
 let Destaques = document.querySelector(".drop2");
+let ShoppingCartOpen = document.querySelector(".shoppingCartOpen");
+let ShoppingCart = document.querySelector(".shoppingCart");
+
 
 
 function click (){
     navList.classList.toggle("active")
+    ShoppingCart.classList.remove("shoppingCartActive")
 }
 function click2 (){
     Categorias.classList.toggle("dropdown")
@@ -13,6 +17,13 @@ function click2 (){
 function click3 (){
     Destaques.classList.toggle("dropdown")
 }
+
+function click4 (){
+    ShoppingCart.classList.toggle("shoppingCartActive")
+    navList.classList.remove("active")
+}
+
 mobileMenu.addEventListener("click", click)
 Categorias.addEventListener("click", click2)
 Destaques.addEventListener("click", click3)
+ShoppingCartOpen.addEventListener("click", click4)
